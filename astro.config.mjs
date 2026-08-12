61eb4ead4b6fff716b35a551852f88eb0711b09c
yashsakhlecha <mirkashco.in@gmail.com>
2026-08-12 14:15:40 +0530

    Fav icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://www.mirkash.com',
+  site: 'https://mirkash.com',
   output: 'server',
   adapter: vercel(),
   trailingSlash: 'never',
